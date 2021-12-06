0c170dba2940356272acac0e7ce3f9a7d6de0cef
Rahul Srivastava <rahul.openstack@gmail.com>
2021-12-07 01:34:38 +0530

    initial commit
    initial commit

---
+++ instances.py
@@ -0,0 +1,11 @@
+import module
+
+# Created a class object
+object = module.GFG()
+
+# Calling and printing class methods
+print(object.add(15,5))
+print(object.sub(15,5))
+
+# Calling the function
+module.method()
---
+++ module.py
@@ -0,0 +1,13 @@
+class GFG:
+
+    # methods
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+
+# explicit function
+def method():
+    print("GFG")
